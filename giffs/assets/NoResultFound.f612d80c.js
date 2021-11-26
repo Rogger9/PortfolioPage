3255aa214aab7ed4c810506acbd5bff45fee8a09
Roger9 <gutierrez.rog12@gmail.com>
2021-11-26 17:03:27 -0300

    Fix routes

--- giffs/assets/NoResultFound.f612d80c.js
+++
@@ -1,1 +0,0 @@
-import{a as s}from"./index.61cbec1b.js";import"./vendor.e9d19354.js";const d=({keyword:o})=>s("h2",{children:["Sorry! No ",o," results found"]});export{d as default};
---
+++ giffs/assets/NoResultFound.ffb3c469.js
@@ -0,0 +1,1 @@
+import{a as s}from"./index.af2a2175.js";import"./vendor.e9d19354.js";const d=({keyword:o})=>s("h2",{children:["Sorry! No ",o," results found"]});export{d as default};
